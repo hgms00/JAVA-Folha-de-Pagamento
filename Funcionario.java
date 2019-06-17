16905dd612b0483c80326a702b29eebb166ce8af
hgms00 <48159371+hgms00@users.noreply.github.com>
2019-06-17 14:54:48 -0300

    Create Funcionario.java

---
+++ Funcionario.java
@@ -0,0 +1,50 @@
+public class Funcionario {
+
+    private static String[] name;
+    private static String[] adress;
+    private static int ID;
+    private static double salary;
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void getName(int id)
+    {
+        return name;
+    }
+
+    public void setAdress(String name)
+    {
+        this.adress=adress;
+    }
+    public void getAdress(int id)
+    {
+        return adress;
+    }
+
+    public void setSalary(int id)
+    {
+        this.salary=salary;
+    }
+    public void getSalary()
+    {
+        return salary;
+    }
+
+    public void pagarEmpregado()
+    {
+
+    }
+
+    public void getID()
+    {
+        return ID;
+    }
+    public void setID(int id)
+    {
+        this.ID=id;
+    }
+
+
+}
