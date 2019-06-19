ce83ac66ece8467c776fdcd651ee5ef7236d5363
hgms00 <48159371+hgms00@users.noreply.github.com>
2019-06-19 10:42:16 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,60 +1,116 @@
 public class Funcionario {
 
-    private static String[] name;
-    private static String[] adress;
+    private static String name;
+    private static String adress;
     private static int ID;
     private static double salary;
     private static int type;
+    private static boolean existe = false;
+
+    private static int tipo_receber;
+    /*tipo_receber
+    1 -> semanalmente
+    2 -> mensalmente
+    3 -> semanas determinadas
+     */
+
+    //mensal
+    private static int mensal_data_receber;
+
+    //semanal
+    private static int a_cada_semana;
+    private static int semanal_dia_receber;
+
+    //sindicato
+    private static int sindicato;
+
+
+
+
 
     /* 1 --> horista
        2 --> salaried
        3 --> commisioned
      */
 
-    public void setName(String name)
-    {
-        this.name=name;
+    public static int getSindicato() {
+        return sindicato;
     }
-    public void getName(int id)
+
+    public static void setSindicato(int sindicato) {
+        Funcionario.sindicato = sindicato;
+    }
+
+    public static void setPayment(int type)
     {
+        if(type==1)
+        {
+            tipo_receber = 1;
+            a_cada_semana = 1;
+            semanal_dia_receber = 5;
+            mensal_data_receber=-1;
+        }
+        else if(type==2)
+        {
+            tipo_receber=2;
+            mensal_data_receber=30;
+            a_cada_semana=-1;
+            semanal_dia_receber=-1;
+        }
+        else if(type==3)
+        {
+            tipo_receber=3;
+            mensal_data_receber=-1;
+            a_cada_semana=2;
+            semanal_dia_receber=5;
+        }
+    }
+
+    public static String getName() {
         return name;
     }
 
-    public void setAdress(String name)
-    {
-        this.adress=adress;
+    public static void setName(String name) {
+        Funcionario.name = name;
     }
-    public void getAdress(int id)
-    {
+
+    public static String getAdress() {
         return adress;
     }
 
-    public void setSalary(double salary)
-    {
-        this.salary=salary;
+    public static void setAdress(String adress) {
+        Funcionario.adress = adress;
     }
-    public void getSalary()
-    {
+
+    public static int getID() {
+        return ID;
+    }
+
+    public static void setID(int ID) {
+        Funcionario.ID = ID;
+    }
+
+    public static double getSalary() {
         return salary;
     }
 
-    public void pagarEmpregado()
-    {
-
+    public static void setSalary(double salary) {
+        Funcionario.salary = salary;
     }
 
-    public void getID()
-    {
-        return ID;
-    }
-    public void setID(int id)
-    {
-        this.ID=id;
-    }
-    public void setType(int type)
-    {
-        this.type=type;
+    public static int getType() {
+        return type;
     }
 
+    public static void setType(int type) {
+        Funcionario.type = type;
+    }
 
+    public static boolean isExiste() {
+        return existe;
+    }
+
+    public static void setExiste(boolean existe) {
+        Funcionario.existe = existe;
+    }
 }
