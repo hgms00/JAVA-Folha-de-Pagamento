a0d47406b1dbdd5e054feca20ef13e703aba01da
hgms00 <48159371+hgms00@users.noreply.github.com>
2019-06-19 11:08:56 -0300

    Update main.java

--- main.java
+++ main.java
@@ -2,18 +2,35 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static int buscarFuncionario(int id,int qt_funcionario,Funcionario employee)
+    {
+        int i;
+
+        for(i=1;i<=qt_funcionario;i++)
+        {
+            if(employee[i].getID==id)
+            {
+                return i;
+            }
+        }
+    }
+
 
      public static void main(String[] args)
     {
          Funcionario employee[] = new Funcionario[1000];
          int command = 1;
-         int qt_funcionario = 0;
          String name;
          String adress;
          double salary;
          int type;
          int id;
+         int qt_funcionario = 0;
+         int id_sindicato;
          int sindicato;
+         int indice;
+         double taxa_sindicato;
+
 
 
         while(command!=0)
@@ -33,6 +50,7 @@ public class Main {
 
             Scanner input = new Scanner(System.in);
             command = input.nextInt();
+            input.nextLine();
 
 
             switch(command)
@@ -90,6 +108,18 @@ public class Main {
 
                     sindicato = input.nextInt();
                     employee[qt_funcionario].setSindicato(sindicato);
+
+                    if(sindicato == 1)
+                    {
+                        System.out.println("Digite a taxa mensal do sindicato");
+                        taxa_sindicato = input.nextDouble();
+                        employee[qt_funcionario].setTaxa_sindical(taxa_sindicato);
+
+
+                        id_sindicato = (2019*10) + qt_funcionario;
+                        employee[qt_funcionario].setId_sindicato(id_sindicato);
+                        System.out.printf("--> O ID do sindicato do funcionário é : %d\n",id_sindicato);
+                    }
                     //--------------------------------------------
 
 
@@ -97,7 +127,20 @@ public class Main {
                     id = (2019*1000) + qt_funcionario;
                     employee[qt_funcionario].setID(id);
 
-                    System.out.printf("--> O ID de funcionário é : %d",id);
+                    System.out.printf("--> O ID de funcionário é : %d\n",id);
+
+                    break;
+                case 2:
+
+                    System.out.println("Digite o ID do funcionário que vocẽ quer remover");
+                    id = nextInt();
+
+                    indice = buscarFuncionario(id,qt_funcionario,employee);
+
+                    if(employee[indice].isExiste()==false)
+                    {
+                        System.out.println("O funcionário não existe");
+                    }
 
 
 
