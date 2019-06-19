7d3fa6ccb98c88db1aa3fa1e060b270086d8f896
hgms00 <48159371+hgms00@users.noreply.github.com>
2019-06-19 15:51:43 -0300

    Create Horista.java

---
+++ Horista.java
@@ -0,0 +1,12 @@
+public class Horista extends Funcionario{
+
+    //semanal
+    private static int a_cada_semana;
+    private static int semanal_dia_receber;
+
+    private static int horas_diarias;
+
+    public static void setHoras_diarias(int horas_diarias) {
+        Horista.horas_diarias = horas_diarias;
+    }
+}
