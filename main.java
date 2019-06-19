ddf9e67ddabaaf5cb8b3cfa3703b5f82f63933e0
hgms00 <48159371+hgms00@users.noreply.github.com>
2019-06-19 10:41:56 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,79 +1,110 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
 
 
-
-
-    private static void main(String[] args)
+     public static void main(String[] args)
     {
-
          Funcionario employee[] = new Funcionario[1000];
-            private static int command = 1;
-            private static int qt_funcionario = 0;
-            private static String[] name;
-            private static String[] adress;
-            private static double salary;
-            private static int type;
-            private static int id;
+         int command = 1;
+         int qt_funcionario = 0;
+         String name;
+         String adress;
+         double salary;
+         int type;
+         int id;
+         int sindicato;
 
 
-            while(command!=0)
+        while(command!=0)
+        {
+            System.out.println("O que você deseja fazer?\n");
+            System.out.println("1. Adicionar um novo funcionário");
+            System.out.println("2. Remover um funcionário existente");
+            System.out.println("3. Lançar um Cartão de Ponto");
+            System.out.println("4. Lançar um Resultado Venda");
+            System.out.println("5. Lançar uma taxa de serviço");
+            System.out.println("6. Alterar detalhes de um empregado");
+            System.out.println("7. Rodar a folha de pagamento para hoje");
+            System.out.println("8. Undo/redo");
+            System.out.println("9. Agenda de pagamento");
+            System.out.println("10. Criar nova agenda de pagamento");
+            System.out.println("0. Sair");
+
+            Scanner input = new Scanner(System.in);
+            command = input.nextInt();
+
+
+            switch(command)
             {
-                System.out.println("O que você deseja fazer?\n");
-                System.out.println("1. Adicionar um novo funcionário");
-                System.out.println("2. Remover um funcionário existente");
-                System.out.println("3. Lançar um Cartão de Ponto");
-                System.out.println("4. Lançar um Resultado Venda");
-                System.out.println("5. Lançar uma taxa de serviço");
-                System.out.println("6. Alterar detalhes de um empregado");
-                System.out.println("7. Rodar a folha de pagamento para hoje");
-                System.out.println("8. Undo/redo");
-                System.out.println("9. Agenda de pagamento");
-                System.out.println("10. Criar nova agenda de pagamento");
-                System.out.println("0. Sair");
+                case 1:
+                    qt_funcionario++;
+                    employee[qt_funcionario] = new Funcionario();
 
-                Scanner input = new Scanner(System.in);
-                command = input.nextInt();
+                    //--------------------------------------------
+                    System.out.println("Digite o nome do funcionário");
+                    //--------------------------------------------
 
 
-                switch(command)
-                {
-                    case 1:
-                        qt_funcionario++;
-                        Funcionario[qt_funcionario] = new Funcionario();
+                    name = input.nextLine();
+                    employee[qt_funcionario].setName(name);
 
-                        System.out.println("Digite o nome do funcionário");
-                        name = input.nextLine();
-                        employee[qt_funcionario].setName(name);
+                    //--------------------------------------------
+                    System.out.println("Digite o endereço do funcionário");
+                    //--------------------------------------------
 
-                        System.out.println("Digite o endereço do funcionário");
-                        adress = input.nextLine();
-                        employee[qt_funcionario].setAdress(adress);
+                    adress = input.nextLine();
+                    employee[qt_funcionario].setAdress(adress);
 
-                        System.out.println("Selecione o tipo do funcionário");
-                        System.out.println("1 --> Horista");
-                        System.out.println("2 --> Assalariado");
-                        System.out.println("3 --> Comissionado");
+                    //--------------------------------------------
+                    System.out.println("Selecione o tipo do funcionário");
+                    System.out.println("1 --> Horista");
+                    System.out.println("2 --> Assalariado");
+                    System.out.println("3 --> Comissionado");
+                    //--------------------------------------------
 
-                        type = input.nextInt();
-                        employee[qt_funcionario].setType(type);
 
-                        System.out.println("Digite o salário do funcionário");
-                        salary = input.nextDouble();
-                        employee[qt_funcionario].setSalary(salary);
+                    type = input.nextInt();
+                    employee[qt_funcionario].setType(type);
 
-                        id = (2019*1000) + qt_funcionario;
-                        employee[qt_funcionario].setID(id);
 
-                        System.out.printf("--> O ID de funcionário é : %d",id);
+                    //Setando o pagamento e a existência
+                    employee[qt_funcionario].setPayment(type);
+                    employee[qt_funcionario].setExiste(true);
 
 
 
-                        break;
-                }
+                    //--------------------------------------------
+                    System.out.println("Digite o salário do funcionário");
+                    //--------------------------------------------
 
+
+                    salary = input.nextDouble();
+                    employee[qt_funcionario].setSalary(salary);
+
+
+                    //--------------------------------------------
+                    System.out.println("O funcionário pertence ao sindicato?");
+                    System.out.println("0 --> NÃO");
+                    System.out.println("1 --> SIM");
+
+                    sindicato = input.nextInt();
+                    employee[qt_funcionario].setSindicato(sindicato);
+                    //--------------------------------------------
+
+
+                    //----------Setando o ID---------------------
+                    id = (2019*1000) + qt_funcionario;
+                    employee[qt_funcionario].setID(id);
+
+                    System.out.printf("--> O ID de funcionário é : %d",id);
+
+
+
+                    break;
             }
+
+        }
     }
 
 }
