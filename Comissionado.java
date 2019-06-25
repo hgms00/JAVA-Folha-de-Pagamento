2c633caa0368ea89cc32a279dc54ed83701e13ca
hgms00 <48159371+hgms00@users.noreply.github.com>
2019-06-25 14:38:29 -0300

    Update Comissionado.java

--- Comissionado.java
+++ Comissionado.java
@@ -1,8 +1,9 @@
 public class Comissionado extends Funcionario {
 
-     private static float percentual;
-     private static double salary;
-     private static double[] valor_venda = new double[31];
+    private static float percentual;
+    private static double salary;
+    private static double[] valor_venda = new double[31];
+    private static double comissao = 0;
 
     public static float getPercentual() {
         return percentual;
@@ -20,6 +21,11 @@ public class Comissionado extends Funcionario {
         Comissionado.salary = salary;
     }
     public static void setValor_venda(double valor_venda,int dia) {
-        Comissionado.valor_venda[dia] = valor_venda;
+        Comissionado.valor_venda[dia] += valor_venda;
+        Comissionado.comissao += (percentual/100)*valor_venda;
+    }
+
+    public static double getComissao() {
+        return comissao;
     }
 }
